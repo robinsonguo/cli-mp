c2720a6b137097c79b12a86d87a0726bc7fa8830
richard <gzq8575@qq.com>
2024-02-24 21:54:59 +0800

    引入arg库处理命令行参数

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log("hello tool");
+const arg = require("arg");
+
+try {
+  const args = arg({
+    "--start": Boolean,
+    "--build": Boolean,
+  });
+
+  if (args["--start"]) {
+    console.log("starting the app");
+  }
+} catch (e) {
+  console.log(e.message);
+  console.log();
+  usage();
+}
+
+function usage() {
+  console.log(`tool [CMD]
+  --start\tStarts the app
+  --build\tBuilds the app`);
+}
